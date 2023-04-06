e80421d923a4646987ea9a74ea37f8440e416bb9
Lucas Mendonça <79822706+iaeluk@users.noreply.github.com>
2023-04-06 14:19:54 -0300

    New fields

--- api/dividends.go
+++ api/dividends.go
@@ -12,14 +12,16 @@ import (
 type Fundo struct {
 	Nome               string `json:"nome"`
 	Codigo             string `json:"codigo"`
-	Preco              string `json:"preco"`
 	Dividendo          string `json:"dividendo"`
-	LiquidezDiaria     string `json:"liquidezDiaria"`
 	DividendYield      string `json:"dividendYield"`
-	PatrimonioLiquido  string `json:"patrimonioLiquido"`
+	PVP                string `json:"p/vp"`
+	Preco              string `json:"preco"`
 	ValorPatrimonial   string `json:"valorPatrimonial"`
 	RentabilidadeNoMes string `json:"rentabilidadeNoMes"`
-	PVP                string `json:"p/vp"`
+	CotasEmitidas      string `json:"cotasEmitidas"`
+	Segmento           string `json:"segmento"`
+	LiquidezDiaria     string `json:"liquidezDiaria"`
+	PatrimonioLiquido  string `json:"patrimonioLiquido"`
 }
 
 // FundsApi recebe um código de fundo imobiliário ou fiagro e retorna informações úteis.
@@ -44,17 +46,11 @@ func FundsApi(codigo string) (Fundo, error) {
 		return Fundo{}, fmt.Errorf("erro ao obter o corpo da requisição... %v", err)
 	}
 
-	//código do fundo
-	codigoDoFundo := doc.Find(`#head > div > div > div > div.ticker-wrapper > h1`).Text()
-
 	//nome do fundo
 	nomeDoFundo := doc.Find(`#head > div > div > div > h3`).Text()
 
-	//preço
-	preco := strings.TrimSpace(doc.Find(`#stock-price > span.price`).Text())
-
-	//liquidez diária
-	liquidezDiaria := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(1) .indicator-value`).Text())
+	//código do fundo
+	codigoDoFundo := doc.Find(`#head > div > div > div > div.ticker-wrapper > h1`).Text()
 
 	//último dividendo mensal
 	dividendo := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(2) .indicator-value`).Text())
@@ -62,30 +58,45 @@ func FundsApi(codigo string) (Fundo, error) {
 	//dividend yield
 	dividendYield := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(3) .indicator-value`).Text())
 
-	//patrimônio líquido
-	patrimonioLiquido := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(4) .indicator-value`).Text())
+	//p/vp
+	pvp := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(7) .indicator-value`).Text())
+
+	//preço
+	preco := strings.TrimSpace(doc.Find(`#stock-price > span.price`).Text())
 
 	//valor patrimonial
 	valorPatrimonial := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(5) .indicator-value`).Text())
 
-	//rendabilidade no mês
+	// rendabilidade no mês
 	rentabilidadeNoMes := strings.TrimSpace(doc.Find(`#main-indicators-carousel div:nth-of-type(6) span:nth-of-type(2)`).Text())
 
-	//p/vp
-	pvp := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(7) .indicator-value`).Text())
+	// quantidade de cotas emitidas
+	cotasEmitidas := strings.TrimSpace(doc.Find(`#basic-infos > div > div > div.section-body > div > div:nth-child(1) > ul > li:nth-child(3) > div.text-wrapper > span.description`).Text())
+
+	//segmento
+	segmento := strings.TrimSpace(doc.Find(`#basic-infos > div > div > div.section-body > div > div:nth-child(2) > ul > li:nth-child(4) > div.text-wrapper > span.description`).Text())
+
+	//liquidez diária
+	liquidezDiaria := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(1) .indicator-value`).Text())
+
+	//patrimônio líquido
+	patrimonioLiquido := strings.TrimSpace(doc.Find(`#main-indicators-carousel .carousel-cell:nth-child(4) .indicator-value`).Text())
 
 	fundo := Fundo{
 		Nome:               nomeDoFundo,
 		Codigo:             codigoDoFundo,
-		Preco:              preco,
 		Dividendo:          dividendo,
-		LiquidezDiaria:     liquidezDiaria,
 		DividendYield:      dividendYield,
-		PatrimonioLiquido:  patrimonioLiquido,
+		PVP:                pvp,
+		Preco:              preco,
 		ValorPatrimonial:   valorPatrimonial,
 		RentabilidadeNoMes: rentabilidadeNoMes,
-		PVP:                pvp,
+		CotasEmitidas:      cotasEmitidas,
+		Segmento:           segmento,
+		LiquidezDiaria:     liquidezDiaria,
+		PatrimonioLiquido:  patrimonioLiquido,
 	}
 
 	return fundo, nil
 }
+
